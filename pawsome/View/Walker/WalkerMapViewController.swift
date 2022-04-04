06cd7be8ec233fba1193ffa3d54839f414fdeb3c
Lovice01 <c0835390@mylambton.ca>
2022-04-04 01:47:01 -0400

    walker viewcontroller creation , cooca file

---
+++ pawsome/View/Walker/WalkerMapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WalkerMapViewController.swift
+//  Pawsome
+//
+//  Created by Lovice Sunuwar on 04/04/2022.
+//
+import MapKit
+import UIKit
+
+class WalkerMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
